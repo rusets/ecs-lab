164aad217d359611d620b120793b9ee98518d189
rusets <rusets3@gmail.com>
2025-09-22 19:51:11 -0500

    Split roles: TF uses terraform-role, deploy uses ecs-role; add WhoAmI & destroy mode

--- infra/backend.tf
+++ infra/backend.tf
@@ -3,7 +3,7 @@ terraform {
     bucket       = "tf-state-rusets-097635932419"
     key          = "docker-ecs-deployment/infra.tfstate"
     region       = "us-east-1"
-    use_lockfile = true
+    # use_lockfile = true
     # encrypt   = true  # при желании
   }
 }
